8e9ad6731bc027a6716af671604f9208a49bd044
wkondrac <01158943@pw.edu.pl>
2023-12-04 19:24:00 +0100

    add init files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+import NavComponent from "./components/Nav/NavComponent";
+import { AboutComponent } from './components/About/AboutComponent';
+
+export default function App() {
+  return (
+    <>
+      <NavComponent />
+      <AboutComponent />
+      
+    </>
+  );
+}
---
+++ src/components/About/AboutComponent.jsx
@@ -0,0 +1,13 @@
+import './AboutComponent.css'
+
+
+export function AboutComponent() {
+    return (
+        <div className="row">
+            <label className="labl-about">ABOUT THIS PROJECT</label>
+            <p className='about'>
+                cosaodoasdoasdoodsaodsaodsosdaodsaodsaodsaodsaodsaodsoasodaoasdodsaodsaodsodsaodsaodsaodsoad
+            </p>
+        </div>
+    );
+}
